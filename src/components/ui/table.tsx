b6e94a05b343db9e003147f104faffab797b473b
Gagan Gupta <gagangupta249@gmail.com>
2025-06-15 09:40:33 +0000

    Still having those errors. 1. src/components/ui/table.tsx (61:3) @ _c8

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,29 +20,27 @@ Table.displayName = "Table"
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, children, ...props }, ref) => ( // Destructure children
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>
-    {children} {/* Explicitly render children here */}
-  </thead>
+>(({ className, children, ...props }, ref) => (
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>{children}</thead>
 ))
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <tbody
     ref={ref}
     className={cn("[&_tr:last-child]:border-0", className)}
     {...props}
-  />
+  >{children}</tbody>
 ))
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <tfoot
     ref={ref}
     className={cn(
@@ -50,65 +48,61 @@ const TableFooter = React.forwardRef<
       className
     )}
     {...props}
-  />
+  >{children}</tfoot>
 ))
 TableFooter.displayName = "TableFooter"
 
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, children, ...props }, ref) => ( // Destructure children
+>(({ className, children, ...props }, ref) => (
   <tr
     ref={ref}
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props} // Spread the rest of the props
-  >
-    {children} {/* Explicitly render children here */}
-  </tr>
+    {...props}
+  >{children}</tr>
 ))
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, children, ...props }, ref) => ( // Explicitly destructure children
+>(({ className, children, ...props }, ref) => (
   <th
     ref={ref}
     className={cn(
       "px-4 py-2.5 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
-    {...props} // Spread remaining props
-  >
-    {children} {/* Render children inside the th element */}
-  </th>
+    {...props}
+  >{children}</th>
 ))
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <td
     ref={ref}
     className={cn("px-4 py-2.5 align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
-  />
+  >{children}</td>
 ))
 TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <caption
     ref={ref}
     className={cn("mt-4 text-sm text-muted-foreground", className)}
     {...props}
-  />
+  >{children}</caption>
 ))
 TableCaption.displayName = "TableCaption"
 
