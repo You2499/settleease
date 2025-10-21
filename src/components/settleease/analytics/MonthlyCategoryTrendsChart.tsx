b2fdfa93c10d99d6b750a491e5de39b10b04588d
You2499 <gagangupta249@gmail.com>
2025-10-21 15:52:20 +0530

    refactor(analytics): Adjust MonthlyCategoryTrendsChart layout and legend positioning
    - Increased bottom margin of AreaChart from 10 to 40 to improve chart spacing
    - Adjusted Legend marginTop from '5px' to '0px' to optimize vertical positioning
    - Maintained consistent chart rendering approach with other analytics components

--- src/components/settleease/analytics/MonthlyCategoryTrendsChart.tsx
+++ src/components/settleease/analytics/MonthlyCategoryTrendsChart.tsx
@@ -286,7 +286,7 @@ export default function MonthlyCategoryTrendsChart({
             </CardHeader>
             <CardContent className={ANALYTICS_STYLES.chartContent}>
                 <ResponsiveContainer width="100%" height={380}>
-                    <AreaChart data={chartData} margin={{ top: 20, right: 30, left: 40, bottom: 10 }}>
+                    <AreaChart data={chartData} margin={{ top: 20, right: 30, left: 40, bottom: 40 }}>
                         <CartesianGrid {...ANALYTICS_STYLES.grid} />
                         <XAxis
                             dataKey="period"
@@ -305,7 +305,7 @@ export default function MonthlyCategoryTrendsChart({
                             ]}
                         />
                         <Legend
-                            wrapperStyle={{ ...ANALYTICS_STYLES.legend, marginTop: '5px' }}
+                            wrapperStyle={{ ...ANALYTICS_STYLES.legend, marginTop: '0px' }}
                             iconType="rect"
                         />
                         {topCategories.map((category, index) => (
